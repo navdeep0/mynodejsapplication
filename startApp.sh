ee81c079f4124e8244143a188d6e187671b3618c
navdeep0 <92580632+navdeep0@users.noreply.github.com>
2022-01-23 17:43:07 +0530

    Update startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=dellstore2.cohobyukovto.ap-south-1.rds.amazonaws.com
+export DB_PRD_HOST=dellstore3.ciyfsqlzs54i.ap-south-1.rds.amazonaws.com
 export DB_PRD_USER=dellstoreaws
-export DB_PRD_PASS=dellstoreaws
+export DB_PRD_PASS=navdeep007
 export NODE_HOST=localhost
 export NODE_PORT=8080
 node /myapp/index.js&
